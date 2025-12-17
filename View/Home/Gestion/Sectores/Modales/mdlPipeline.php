524afc052bb6782afbe8a0d81be3e74f33bb4315
Mauri2591 <mauriciorgonzalez25@gmail.com>
2025-12-16 21:32:58 -0300

    Se actualiza el codigo

--- View/Home/Gestion/Sectores/Modales/mdlPipeline.php
+++ View/Home/Gestion/Sectores/Modales/mdlPipeline.php
@@ -1,11 +1,12 @@
-<div class="modal" tabindex="-1" id="ModalVerTecnologiasPipeline">
+<!-- Modal -->
+<div class="modal fade" id="ModalVerTecnologiasPipeline" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title">Crear Pipeline</h5>
             </div>
             <div class="modal-body">
-                <form id="formPipelineHerramienta" action="<?php echo URL."/Controller/ctrProyectos.php?proy=descargarPipeline" ?>" method="post">
+                <form id="formPipelineHerramienta" action="<?php echo URL . "/Controller/ctrProyectos.php?proy=descargarPipeline" ?>" method="post">
 
                     <input type="hidden" hidden id="id_proyecto_gestionado_pipeline" name="id_proyecto_gestionado">
                     <input type="hidden" hidden id="refProy_pipeline" name="refProy">
@@ -16,7 +17,7 @@
                             <option class="text-center fw-bold" value="SEMBREP">Semgrep</option>
                         </select>
                     </div>
-                    
+
                     <div class="modal-footer mt-4 mb-0 pb-0">
                         <button type="button" class="btn btn-sm btn-light" data-bs-dismiss="modal">Close</button>
                         <button name="btnDescargarPipeline" type="submit" class="btn btn-sm btn-primary">Descargar</button>
