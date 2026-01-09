b5c3d9324516965ca9053e315c394b55bc1ddfc8
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-01-09 11:38:22 -0300

    Se actualizo

--- View/Home/Gestion/Integraciones/index.php
+++ View/Home/Gestion/Integraciones/index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['usu_id'])) {
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Gestion - Clientes</h4>
+                        <h4 class="mb-sm-0">Gestion - Integraciones</h4>
                     </div>
                 </div>
             </div>
