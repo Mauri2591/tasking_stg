4a142c9a93556b685ae096d87fa27db28c5f474d
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-01-11 12:46:49 -0300

    Se actualizo

--- View/Home/Gestion/Sectores/GestionarProy/index.php
+++ View/Home/Gestion/Sectores/GestionarProy/index.php
@@ -210,10 +210,10 @@ if (isset($_SESSION['usu_id'])) {
                                     </div>
                                     <div class="col-xl-2 bg-success" style="max-height: 500px;border-radius: 5px;">
                                         <div style="min-height: 250px;">
-                                            <span onclick="agregarUsuario(<?php echo Openssl::get_ssl_decrypt($_GET['p']) ?>)" type="button" style="font-size: .75rem; font-weight: 500;"
+                                            <span style="font-size: .75rem; font-weight: 500;"
                                                 class="badge bg-light border border-dark text-dark mx-2 mt-2 mb-3 d-inline-flex align-items-center gap-1">
                                                 Usuarios Asignados:
-                                                <i id="boton_agregar_usuarios_proy"
+                                                <i onclick="agregarUsuario(<?php echo Openssl::get_ssl_decrypt($_GET['p']) ?>)" type="button"  id="boton_agregar_usuarios_proy"
                                                     class="ri-user-add-line text-danger fw-bold fs-5"
                                                     style="cursor: pointer; display: none;"></i>
                                             </span>
