ad00a529430ee254ac3bc5afb21e92917e5dd932
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-01-29 15:23:07 -0300

    Se actualiza

--- View/Home/Gestion/Clientes/Proyectos/index.php
+++ View/Home/Gestion/Clientes/Proyectos/index.php
@@ -152,7 +152,7 @@ if (isset($_SESSION['usu_id'])) {
                         <div class="tab-pane" id="tab_total" role="tabpanel">
                             <div class="card card-body">
                                 <table id="table_proyectos_total_calidad" style="text-align: center; width: 100%;">
-                                    <div style="display: flex; justify-content: end;">
+                                    <div style="display: flex; justify-content: end; height: 1.5rem;">
                                         <span class="mb-4"><span class="badge bg-primary fs-12">Reporte Clientes</span><i onclick="mdlDescargarExcelProyectosTotal()" class="ri-file-excel-2-fill text-success fs-22" type="button" title="Descargar documento"></i></span>
                                     </div>
                                     <thead style="text-align: center;">
@@ -289,7 +289,7 @@ if (isset($_SESSION['usu_id'])) {
                         <div class="tab-pane fade show active" id="tab_total" role="tabpanel">
                             <div class="card card-body">
                                 <table id="table_proyectos_total_calidad" style="text-align: center; width: 100%;">
-                                    <div style="display: flex; justify-content: end;">
+                                    <div style="display: flex; justify-content: end; height: 1.5rem;">
                                         <span class="mb-4"><span class="badge bg-primary fs-12">Reporte Clientes</span><i onclick="mdlDescargarExcelProyectosTotal()" class="ri-file-excel-2-fill text-success fs-22" type="button" title="Descargar documento"></i></span>
                                     </div>
                                     <thead style="text-align: center;">
