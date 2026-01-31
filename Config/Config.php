4a0efcb6c776e6215b241ca2efc3c88b2d0bcb5a
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-01-31 13:07:42 -0300

    Se actualiza

--- Config/Config.php
+++ Config/Config.php
@@ -12,6 +12,5 @@ define("URL", $_ENV['URL']);
 define("KEY", $_ENV['KEY']);
 define("BASE_URL", getenv('URL'));
 define("BASE_PATH", $_SERVER['DOCUMENT_ROOT'] . "/tasking_stg/");
-define('OSINT_BASE_PATH',rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/tasking_stg'
-);
+
 
