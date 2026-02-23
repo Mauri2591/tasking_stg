0bc8a0703e0ae0c5e252ce07a8d7359f6ff1eaff
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-02-23 10:54:19 -0300

    Se actualiza

--- Model/Timesummary.php
+++ Model/Timesummary.php
@@ -432,7 +432,7 @@ ORDER BY id_proyecto_gestionado ASC";
         $conn = parent::get_conexion();
         $sql = "SELECT usu_id, usu_nom, usu_ape, LOWER(usu_correo) 
             FROM tm_usuario 
-            WHERE est=1 AND sector_id = 1 AND usu_id NOT IN (82,104)"; //que no traiga al usuario dev ni testMauricio
+            WHERE est=1 AND sector_id = ? AND usu_id NOT IN (82,104)"; //que no traiga al usuario dev ni testMauricio
         $stmt = $conn->prepare($sql);
         $stmt->execute([$sector_id]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
