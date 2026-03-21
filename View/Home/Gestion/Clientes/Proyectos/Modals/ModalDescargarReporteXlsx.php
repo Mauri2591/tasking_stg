20dc79a0df74d47d92afdd2203cd5626eb231251
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-03-21 14:22:43 -0300

    Se actualiza

--- View/Home/Gestion/Clientes/Proyectos/Modals/ModalDescargarReporteXlsx.php
+++ View/Home/Gestion/Clientes/Proyectos/Modals/ModalDescargarReporteXlsx.php
@@ -35,7 +35,7 @@
         </form>
         <div class="mt-3 mb-1 mx-2 text-center p-2 border bg-light">
             <small class="text-danger mt-0 fs-12">
-                Este reporte muestra el total de todos los <strong>Proyectos por Cliente.</strong><br> Se excluyen aquellos que hayan sido <strong>Borrados o Finalizados Sin Implementar</strong>.
+                Este reporte muestra el total de todos los <strong>Proyectos de todos los Clientes.</strong><br> Se excluyen aquellos que hayan sido <strong>Borrados o Finalizados Sin Implementar</strong>.
             </small>
         </div>
     </div>
