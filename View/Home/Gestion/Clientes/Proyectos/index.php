bef69f2b694607658dfb03a0b882fb65a936d4bd
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-03-23 17:37:33 -0300

    Se actualiza, se agrega funcionalidad de personal de Calidad pueda Agregarse como PM

--- View/Home/Gestion/Clientes/Proyectos/index.php
+++ View/Home/Gestion/Clientes/Proyectos/index.php
@@ -181,7 +181,7 @@ if (isset($_SESSION['usu_id'])) {
                                     <thead style="text-align: center;">
                                         <tr style="text-align: center;">
                                             <th style="width: 10px;text-align: center;">PRIO</th>
-                                            <th style="width: 250px;text-align: center;">CLIENTE</th>
+                                            <th style="width: 200px;text-align: center;">CLIENTE</th>
                                             <th style="width: 50px;text-align: center;">PM</th>
                                             <th style="width: 10px;text-align: center;">REC</th>
                                             <th style="width: 10px;text-align: center;">RETEST</th>
