b9e87e88436d3c1bcdc7193983688b315741fcc1
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-07-16 10:49:38 -0300

    Se actualiza

--- View/Home/Apps/Timesummary/Consultas.php
+++ View/Home/Apps/Timesummary/Consultas.php
@@ -153,7 +153,7 @@ if (isset($_SESSION['usu_id'])) {
                                         <table style="text-align: center;" id="table_get_tareas_x_usuario_x_usu_id">
                                             <thead>
                                                 <tr>
-                                                    <th style="width: 10%;text-align: center;">CLIENTE</th>
+                                                    <th style="width: 10%;text-align: center;">TITULO</th>
                                                     <th style="width: 5%;text-align: center;">REF</th>
                                                     <th style="width: 5%;text-align: center;">PRODUCTO</th>
                                                     <th style="width: 5%;text-align: center;">TAREA</th>
