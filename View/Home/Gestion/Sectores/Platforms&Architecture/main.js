a6d8c22f18793245eb704b2c82799a4fdd129b9b
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-09-20 15:54:08 -0300

    Se agrega sector Platforms y Architecture

--- View/Home/Gestion/Sectores/Platforms&Architecture/main.js
+++ View/Home/Gestion/Sectores/Platforms&Architecture/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
             type: "post",
             dataType: "json",
             data: {
-                sector_id: 1,
+                sector_id: 8,
                 estados_id: 1
             },
             error: function (e) {}
@@ -75,7 +75,7 @@ $(document).ready(function () {
             type: "post",
             dataType: "json",
             data: {
-                sector_id: 1,
+                sector_id: 8,
                 estados_id: 2
             },
             error: function (e) {}
@@ -132,7 +132,7 @@ $(document).ready(function () {
             type: "post",
             dataType: "json",
             data: {
-                sector_id: 1,
+                sector_id: 8,
                 estados_id: 3
             },
             error: function (e) {}
